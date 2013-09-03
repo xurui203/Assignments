e999ed77896880329528c57632f0f38356c52351
xurui203 <xurui203@gmail.com>
2013-09-03 17:16:50 -0400

    bash assignment

---
+++ bash/log_parse.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+bunzip2 -k chronline.2013.08.27.bz2
+
+grep 'Started GET' chronline.2013.08.27 |
+sed 's/.*Started GET ".*" for \([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*\/\1/' | 
+sort > addresses
+
+ADDRESSES=$(cat addresses | wc =l)
+DUKE_ADDRS=$(grep '153.3' addresses | wc -l)
+
+echo "Total Duke addresses : $DUKE_ADDRS/$ADDRESSES"
+
+
