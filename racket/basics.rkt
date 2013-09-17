3cecc146a0c10ce559d9f20d1e71cb347c26167a
xurui203 <xurui203@gmail.com>
2013-09-17 17:08:54 -0400

    Done with basics

--- racket/basics.rkt
+++ racket/basics.rkt
@@ -35,29 +35,85 @@
 ;;; Return a list with the *first* instance of element a removed if present
 (define (rember lst a) 
   (cond 
-  ((null? lst) lst)
+  ((null? lst) '())
   ((equal? a (car lst)) (cdr lst))
   (else
     (cons (car lst) (rember (cdr lst) a)))))
 
+;;; Return where element is an atom
+(define (atom? a)
+  (not (list? a)))
+
 ;;; Return whether element a is contained in any level of S-expression lst
-(define (member?* a lst) null)
+(define (member?* a lst) 
+  (cond 
+    ((null? lst) #f)
+    ((equal? a (car lst)) #t)
+    ((and (list? (car lst))
+          (member?* a (car lst))) #t)
+    (else
+     (member?* a (cdr lst)))))
+  
+
 
 ;;; Return the intersection of sets set1 and set2
 ;;; set1 and set2 are guaranteed not to contain duplicate elements
-(define (intersect set1 set2) null)
+(define (intersect set1 set2) 
+  (cond
+    ((null? set1) null)
+    (else
+     (let ([rest (intersect (cdr set1) set2)])
+       (if (member? (car set1) set2)
+           (cons (car set1) rest)
+           rest)))))
 
 ;;; Return whether the list contains two equal adjacent elements
-(define (two-in-a-row? lst) null)
+(define (two-in-a-row? lst) 
+  (define (two-in-a-row-helper last lst)
+    (cond 
+      ((null? lst) #f)
+      ((equal? last (car lst)) #t)
+      (else
+       (two-in-a-row-helper (car lst) (cdr lst)))))
+    (and (not (null? lst))
+       (two-in-a-row-helper (car lst) (cdr lst)))
+)
+
+(define (two-in-a-row?* lst)
+  (define (two-in-a-row-helper last lst)
+    (cond
+      ((null? lst) #f)
+      ((equal? last (car lst)) #t)
+      (else
+       (two-in-a-row-helper (car lst) (cdr lst)))))
+  (and (not (null? lst))
+       (two-in-a-row-helper (car lst) (cdr lst))))
 
 ;;; Return the nth element of a list
-(define (nth lst n) null)
+(define (nth lst n) 
+  (cond
+    ((zero? n) (car lst))
+    (else
+     (nth (cdr lst) (- n 1)))))
 
 ;;; Return a list containing the unique elements of lst
-(define (dedup lst) null)
+(define (dedup lst)
+  (cond
+    ((null? lst) null)
+    ((member? (car lst) (cdr lst))
+     (dedup (cdr lst)))
+    (else
+     (cons (car lst) (dedup (cdr lst))))))
 
 ;;; Return a list containing the elements of lst in reverse order
-(define (reverse lst) null)
+(define (reverse lst) 
+  (define (rev-h lst rev)
+    (cond
+      ((null? lst) rev)
+      (else
+       (rev-h (cdr lst) (cons (car lst) rev)))))
+  (rev-h lst null))
+ 
 
 (provide
  sum-to-n
