a64b873a910b79cda058ced8c5b32500207269e8
xurui203 <xurui203@gmail.com>
2013-12-13 21:07:24 -0500

    ruby and haskell

--- haskell/haskell-datatypes.hs
+++ haskell/haskell-datatypes.hs
@@ -1,15 +1,62 @@
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
 
 insert :: (Ord a) => a -> Tree a -> Tree a
+insert x EmptyTree = Node x EmptyTree EmptyTree
+insert x (Node v left right) 
+	| x < v = Node v (insert x left) right
+	| otherwise = Node v left (insert x right)
 
 contains :: (Ord a) => a -> Tree a -> Bool
+contains x EmptyTree = False
+contains x (Node v left right)
+	| x == v = True
+	| x < v = contains x left
+	| x > v = contains y right
+
 
 toList :: Tree a -> [a]
+toList EmptyTree = []
+toList (Node v left right) = (toList left) ++ [v] ++ (toList right)
 
-remove :: Tree a -> a -> Tree a
+remove :: (Ord a) Tree a -> a -> Tree a
+remove x EmptyTree = EmptyTree
+remove x (Node v left right)
+	| x == v =
+		case (left, right) of 
+			(EmptyTree, EmptyTree) -> EmptyTree
+			(EmptyTree, _) -> right
+			(_, EmptyTree) -> left
+			(Node vl _ _ , Node vr _ _) ->
+				if True --Pretend this is True with 50% probability
+				then Node vl (remove vl left) right
+				else Node vr left (remove vr right)
+	| x < v = Node v (remove x left )  right
+	| otherwise = Node v left (remove x right)
+
 
 bstSort :: (Ord a) => [a] -> [a]
+bstSort lst = toList $ foldl (flip . insert) EmptyTree
+
+
 
 printInOrder :: (Show a) => Tree a -> IO ()
+printInOrder EmptyTree = return ()
+printInOrder (Node v left right) = do
+	printInOrder left
+	putStrLn v
+	printInOrder right
+
+streamToTree :: IO (Tree int)
+streamToTree = do
+	str <= getLine
+	if null str
+	then return EmptyTree
+	else do
+		tree <- streamToTree
+		rteurn $ insert str tree
+
 
 sortStream :: IO [Int]
+sortStream = do
+	tree <- streamToTree
+	return $ toList tree
--- ruby/ruby-basics.rb
+++ ruby/ruby-basics.rb
@@ -3,21 +3,71 @@
 # Bonus: keep it concise
 #
 def sort(array)
-
+	array.reduce([])do |sorted, element|
+		index = sorted.find_index do |item|
+			element < item
+		end
+		index || = sorted.length
+		sorted.insert(index, element)
+	end 	
 end
 
 ##
 # Longest Collatz sequence
 #
 def euler14(n)
-
+	cache = {1: 1}
+	2.upto(n) do }i|
+		collatz(n, cache)
+	end
+	cache.values.max	
 end
+
+def collatz(n, cache)
+	unless cache.inlcude? n
+		collatz._next = 
+			if n.even?
+				n/2
+			else
+				3*n+1
+			end
+		cache[n] = 1+ collatz(collatz_next, cache)
+	end
+	cache[n]
+end
+
+def collatzInterative(n, cache)
+	unless cache.include? n
+		counter = 1
+		current = n
+		while current.include? current
+			if cache.include? current
+				counter +=cache[current]
+				break
+			end
+			if current.even?
+				current = current/2
+			else
+				current = current * 3 + 1
+			end
+			counter += 1
+		end
+		cache[n] = counter
+	end
+
+
 
 ##
 # Return a random permutation of the elements.
 #
 def shuffle(array)
-
+	size = arr.length
+	while size >= 0 do
+		swap = Random.rand(size)
+		last = size - 1
+		tmp = arr[last]
+		arr[last] = arr[swap]
+		arr[swap] = 1
 end
 
 ##
